a48cd5fb035cb84c625635430674e016c6791cc5
Alex Tsan <tsl4297a@ens.fsi>
2023-09-21 15:23:54 +0200

    majTP1

---
+++ src/personnages/Druide.java
@@ -0,0 +1,49 @@
+package personnages;
+
+public class Druide {
+	private String nom; 
+	private int effetPotionMax;
+	private int effetPotionMin;
+	
+	public Druide(String nom, int effetPotionMax, int effetPotionMin) {
+		super();
+		this.nom = nom;
+		this.effetPotionMax = effetPotionMax;
+		this.effetPotionMin = effetPotionMin;
+		parler("Je suis le druide " + nom + "effet potion min" + effetPotionMax + "effet potion max " + effetPotionMax);
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getEffetPotionMax() {
+		return effetPotionMax;
+	}
+
+	public void setEffetPotionMax(int effetPotionMax) {
+		this.effetPotionMax = effetPotionMax;
+	}
+
+	public int getEffetPotionMin() {
+		return effetPotionMin;
+	}
+
+	public void setEffetPotionMin(int effetPotionMin) {
+		this.effetPotionMin = effetPotionMin;
+	}
+	
+	public String prendreParole() {
+		return "Le duide " +nom + ": ";
+		
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+	}
+	
+}
