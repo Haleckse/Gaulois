4ed0bf984f99081fa4afc5dec8f95fed778e39aa
Alex Tsan <tsl4297a@ensfsi.onmicrosoft.com>
2023-10-03 17:46:26 +0200

    TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,14 @@ package personnages;
 public class Romain {
 	private String nom; 
 	private int force;
+	private Equipement[] equipement; 
+	private int nbEquipement = 0 ;  
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert(force >= 0) : "Entrez une valeur >= 0"; 
+		
 		
 	}
 	public String getNom() {
@@ -25,6 +29,8 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert(force >= 0) : "La force d'un romain est toujours positive"; 
+		int forceAvantCoup = force; 
 		force -= forceCoup;
 		if (force > 0 ) {
 			parler("Aie");
@@ -32,7 +38,21 @@ public class Romain {
 		else {
 			parler("J'abandonne");
 		}
+		assert(forceAvantCoup > force) : "La force du romain doit diminuer"; 
 	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+		case 2 :
+			System.out.println("Le soldat " + nom + " est deja bien protégé !");
+		case 1 :
+			if (equipement[0] == equipement) {
+				System.out.println("Le soldat Minus possède déjà un casque.");
+			}
+		}
+		
+		
+	}	
 	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
